9aea114b692a7c12a4b69ea0b6da43b7ce6f908d
drtikv <drtikv@email.cz>
2017-03-02 18:17:22 +0100

    los -> ob

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
-$(call inherit-product, device/lge/d855/full_d855.mk)
+$(call inherit-product, device/lge/d855/ob_d855.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OblivioN stuff.
+$(call inherit-product, vendor/ob/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_d855
+PRODUCT_NAME := ob_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
